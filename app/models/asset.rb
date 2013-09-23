b8a6afed5c737c1791bd691db581b5edeca182a3
Adam Perlis <adam.perlis@gmail.com>
2013-09-23 08:42:05 -0400

    removed asset.rb

--- app/models/asset.rb
+++
@@ -1,6 +0,0 @@
-class Asset < ActiveRecord::Base
-	belongs_to :post
-	has_attached_File :asset, :styles => { :large => "1920x1080", :medium => "1280x720>", :thumb "260x260>" }	
-
-  attr_accessible :asset_content_type, :asset_file_name, :asset_file_size, :asset_updated_at, :project_id
-end
