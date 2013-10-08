3236715ff3ae3312c144bcc0e0037d3d9ad018c1
Adam Perlis <adam.perlis@gmail.com>
2013-10-08 17:36:34 -0400

    fixed other mogration

--- db/migrate/20130926035316_change_avatar_to_filepicker_avatar.rb
+++ db/migrate/20130926035316_change_avatar_to_filepicker_avatar.rb
@@ -1,7 +1,6 @@
 class ChangeAvatarToFilepickerAvatar < ActiveRecord::Migration
 	change_table :users do |t|
 		t.remove :avatar_file_name
-		t.remove :avatar_file_content_type
 		t.remove :avatar_file_size
 		t.remove :avatar_updated_at
 		t.string :filepicker_url_avatar
