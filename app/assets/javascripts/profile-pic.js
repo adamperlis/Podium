58f5574539f5c01fbfb7736cf5f79e9338d277c4
Adam Perlis <adam.perlis@gmail.com>
2013-11-04 17:57:02 -0500

    Added profile pic validation

--- app/assets/javascripts/profile-pic.js
+++ app/assets/javascripts/profile-pic.js
@@ -18,4 +18,14 @@ $(function (){
         services:['COMPUTER', 'DROPBOX', 'GOOGLE_DRIVE', 'URL', 'FACEBOOK', 'INSTAGRAM']
     }, filepicker_avatar);
   });
+
+  //Validates the profile picture URL is present.
+  $("#register").submit(function(e){
+    if (!$("input[id=avatar-url]").val()) {
+      $("#register").prepend("<div class='alert-box alert error'>Please upload a profile picture</div>");
+      return false
+    }
+   
+  });
+
 });
