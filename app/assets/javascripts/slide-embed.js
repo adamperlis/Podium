bc0937c6167d4f056c25055a846813aa1eda6c26
Adam Perlis <adam.perlis@gmail.com>
2013-11-25 18:40:14 -0500

    Removed iFrame from EDIT

--- app/assets/javascripts/slide-embed.js
+++ app/assets/javascripts/slide-embed.js
@@ -25,14 +25,8 @@ function setupEmbed(url) {
 	$('#current-slide').html("<div class='blank-slide'></div>");
 	if (url) {
 		var contains = url;
-		
-		//trying to get iFrame to work.
-		if (!contains.indexOf('youtube' + 'vimeo') > -1){
-			$('#current-slide').html('<iframe src=' + url + '></iframe>');
-		}else{
-			var pop = Popcorn.smart( "#current-slide .blank-slide", url );
-			var pop = Popcorn.smart( "#empty .blank", url ); //trying to get embeded videos to show up in slide organizer
-		}
-
+			
+		var pop = Popcorn.smart( "#current-slide .blank-slide", url );
+		var pop = Popcorn.smart( "#empty .blank", url ); //trying to get embeded videos to show up in slide organizer
 	}
 }
