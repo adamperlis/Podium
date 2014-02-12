c552d36dd79127e57a2937768226243e743e2d1b
Adam Perlis <adam.perlis@gmail.com>
2014-02-12 18:15:54 -0500

    removed duplicate migration

--- db/migrate/20130917084738_add_attachment_avatar_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentAvatarToUsers < ActiveRecord::Migration
-  def up
-    add_column :users, :filepicker_url_avatar, :string
-  end
-
-  def down
-    remove_column :users, :filepicker_url_avatar
-  end
-end
