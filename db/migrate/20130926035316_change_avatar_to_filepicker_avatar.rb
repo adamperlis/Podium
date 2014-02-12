7ddb0e6d5b212b3f75a43111adc6b280087707a2
Adam Perlis <adam.perlis@gmail.com>
2014-02-12 18:20:51 -0500

    removed non-existant removals

--- db/migrate/20130926035316_change_avatar_to_filepicker_avatar.rb
+++ db/migrate/20130926035316_change_avatar_to_filepicker_avatar.rb
@@ -1,8 +1,5 @@
 class ChangeAvatarToFilepickerAvatar < ActiveRecord::Migration
 	change_table :users do |t|
-		t.remove :avatar_file_name
-		t.remove :avatar_file_size
-		t.remove :avatar_updated_at
 		t.string :filepicker_url_avatar
   end
 end
