5ac00f86d8ed9aa004ba5818ae8813b71bcd9598
Adam Perlis <adam.perlis@gmail.com>
2014-04-09 20:00:48 -0400

    added referral code and user project limit to site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :capture_referral, unless: :signed_in?
+
+  def capture_referral
+  	session[:referral] = params[:referral] if params[:referral]
+  end
+
   def sign_user_out
     sign_out if signed_in?
     redirect_to :root
