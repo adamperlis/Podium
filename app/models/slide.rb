fce3b0fbef3a8356cf37cb24f5f67c3de2106c2b
Adam Perlis <adam.perlis@gmail.com>
2014-05-08 20:42:23 -0400

    fixed cloudconvert and refactored slides model for pdf conversion

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,33 +1,58 @@
 class Slide < ActiveRecord::Base
 	acts_as_list
 
-  attr_accessible :filepicker_url, :filepicker_url_thumb, :mimetype, :embed_code
+  attr_accessible :filepicker_url, :filepicker_url_thumb, :mimetype, :embed_code, :project_id
 
   belongs_to :project
 
   def self.new_from_pdf(pdf_url, project_id)
-  	image = Magick::ImageList.new
-    urlpdf = open(pdf_url)
-    image.from_blob(urlpdf.read)
+    full_pdf_url = prepend_http_if_needed(pdf_url)   
+    image = create_imagelist_from_pdf(full_pdf_url)
     slides_array = []
+    filename = full_pdf_url.split("/")[-1].split(".pdf")[0]
+
+    (0..(image.length - 1)).each do |i| 
+      obj = self.save_to_s3("#{filename}-#{i}.jpg", image[i])
+      slide = self.new({
+        mimetype: "image/jpg",
+        project_id: project_id,
+        filepicker_url: obj.url_for(:read, :expires => 60*60*24*365*20).to_s,
+        filepicker_url_thumb: obj.url_for(:read, :expires => 60*60*24*365*20).to_s
+      })
+      slide.save!
+      slides_array << slide
+    end
+    return slides_array
+  end
+  private
+
+  def self.prepend_http_if_needed(url)
+    u = URI.parse(url)
+    if(!u.scheme)
+      url = "http:#{url}"
+    end
+    return url
+  end
+
+  def self.save_to_s3(filename, image)
+    obj = s3_bucket.objects[filename]
+    image.format = "JPEG"
+    obj.write(image.to_blob)
+    return obj
+  end
+
+  def self.s3_bucket
     s3 = AWS::S3.new({
       :access_key_id     => ENV['S3_KEY'],
       :secret_access_key => ENV['S3_SECRET']
     })
     bucket = s3.buckets['getpodium-media']
-    filename = pdf_url.split("/")[-1].split(".pdf")[0]
+  end
 
-    (0..(image.length - 1)).each do |i| 
-      obj = bucket.objects["#{filename}-#{i}.jpg"]
-      image[i].format = "JPEG"
-      obj.write(image[i].to_blob)
-      slide = self.new(mimetype: "image/jpg")
-      slide.project_id = project_id
-      slide.filepicker_url = obj.url_for(:read, :expires => 60*60*24*365*20).to_s
-      slide.filepicker_url_thumb = obj.url_for(:read, :expires => 60*60*24*365*20).to_s
-      slide.save!
-      slides_array << slide
-    end
-    slides_array
+  def self.create_imagelist_from_pdf(pdf)
+    image = Magick::ImageList.new
+    urlpdf = open(pdf)
+    image.from_blob(urlpdf.read)
+    return image
   end
 end
