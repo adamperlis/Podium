379b79265021a9b91d0da9e05d5daa31438dff77
Adam Perlis <adam.perlis@gmail.com>
2014-05-21 16:54:18 -0400

    made all mailer actions go to mail folder in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Omrails::Application.configure do
 
   config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :file
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
 end
