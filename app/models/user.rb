d127c47dd6fbd5df1ca1148c3b4019e0989fc0d9
Adam Perlis <adam.perlis@gmail.com>
2014-05-21 17:47:06 -0400

    added Devise confirmable to Podium and fixed alert messages to include stroke and attention symbol

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, 
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :filepicker_url_avatar, :referral_code, :project_limit
