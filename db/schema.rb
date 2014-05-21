913145f9057a022e52c95298994051e98f252fcf
Adam Perlis <adam.perlis@gmail.com>
2014-05-21 17:47:06 -0400

    added Devise confirmable to Podium and fixed alert messages to include stroke and attention symbol

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140423224803) do
+ActiveRecord::Schema.define(:version => 20140521205659) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -98,8 +98,12 @@ ActiveRecord::Schema.define(:version => 20140423224803) do
     t.string   "filepicker_url_avatar"
     t.integer  "project_limit",          :default => 5
     t.string   "referral_code"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
