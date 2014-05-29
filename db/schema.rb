c7aadb53b2044c1b2bb48390f1a9541318fe57a7
Adam Perlis <adam.perlis@gmail.com>
2014-05-29 19:03:43 -0400

    Added Super User as Admin.
    -Create new user
    -Confirm user
    -Set Admin boolean flag to true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140521205659) do
+ActiveRecord::Schema.define(:version => 20140529225539) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -82,8 +82,8 @@ ActiveRecord::Schema.define(:version => 20140521205659) do
   add_index "slides", ["project_id"], :name => "index_slides_on_project_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -92,8 +92,8 @@ ActiveRecord::Schema.define(:version => 20140521205659) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "name"
     t.string   "filepicker_url_avatar"
     t.integer  "project_limit",          :default => 5
@@ -101,8 +101,10 @@ ActiveRecord::Schema.define(:version => 20140521205659) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean  "admin",                  :default => false
   end
 
+  add_index "users", ["admin"], :name => "index_users_on_admin"
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
