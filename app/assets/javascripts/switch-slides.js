7118aa5463b8450f98c4a307bd1f2e23160ac6af
Adam Perlis <adam.perlis@gmail.com>
2014-07-24 19:04:41 -0400

    fixed multidelete!

--- app/assets/javascripts/switch-slides.js
+++ app/assets/javascripts/switch-slides.js
@@ -39,8 +39,7 @@ $(document).ready(function(){
 	    var current_slide = $('.slide img.ui-selected').parents('li');
 	    var prev_slide = current_slide.prev(".slide");
 	    var next_slide = current_slide.next(".slide");
-	    
-
+	   
 	    if (e.keyCode == 38 && prev_slide.length) { 
 	    
       	prev_slide.find("img").addClass("ui-selected");
@@ -66,11 +65,24 @@ $(document).ready(function(){
     	}
 
     	if (e.keyCode == 8 && current_slide.length && confirm("Are you sure you want to delete this slide?")) { 
-    		
+    		    
   					e.preventDefault();
-        		deleteSlide(current_slide.data("id"), function(){
-              next_slide.find("img").addClass("ui-selected");
-              displayCurrentSlide();
+            next_slide = next_slide.toArray().diff(current_slide.toArray());
+
+            if(next_slide.length == 0) {
+              next_slide = prev_slide.toArray().diff(current_slide.toArray());
+            }
+
+            next_slide = $(next_slide.sort(function(a, b){
+              return parseInt($(a).data("id")) > parseInt($(b).data("id"));
+            }));
+
+            $.each(current_slide, function(index, slide) {
+
+          		deleteSlide($(slide).data("id"), function(){
+                next_slide.find("img").addClass("ui-selected");
+                displayCurrentSlide();
+              });
             });
             
     	}
