8b76da14de8bc14d5ad59f4e399e4b75d7a04b2c
Adam Perlis <adam.perlis@gmail.com>
2014-08-13 17:18:41 -0400

    Added Admin Only before filter to dashboard view plus some CSS changes for it.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_filter :admins_only, only: [:dashboard]
+
   def home
   end
 
@@ -11,25 +13,11 @@ class PagesController < ApplicationController
   	
  	end
 
- 	def self.chart_data(start = 1.year.ago)
-    total_count = total_count_by_month(start)
-    start = start.to_date.beginning_of_month
-    today = Date.today.beginning_of_month
-    range = (start..today).select {|d| d.day == 1}
-      range.map do |month|
-        {
-          created_at: month,
-          total_signups: total_count[month] || 0
-      }
-    end
-  end
+  private
 
-  def self.total_count_by_month(start)
-    signups = where(created_at: start.beginning_of_month..Time.now)
-    signups = @users.group("date_trunc('month', created_at)")
-    signups = @users.select("date_trunc('month', created_at) as created_at, count(*) as count")
-    signups.each_with_object({}) do |signup, counts|
-      counts[signup.created_at.to_date] = signup.count
+  def admins_only
+    if !signed_in? || !current_user.admin?
+      redirect_to root_path, notice: "You don't have access to this page"
     end
   end
 end
